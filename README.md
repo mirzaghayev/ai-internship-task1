11ea0c0a6b6aec47640a622a2211464f26aa4028
mirzaghayev <mirzagamirzagayev07@gmail.com>
2026-07-21 12:32:11 +0400

    Initial commit - Task 1 setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# AI Internship - Task 1: API Integration
+
+## setup
+1. Bu repository-ni klonlayın.
+2. Yuklenecekler:
+   ```bash
+   pip install groq python-dotenv
---
+++ test_api.py
@@ -0,0 +1,13 @@
+import os
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[{"role": "user", "content": "Say hello and tell me you are ready!"}]
+)
+
+print(response.choices[0].message.content)
